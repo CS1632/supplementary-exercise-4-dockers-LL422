728f29daf859a226f7d9253996963b9238723dc9
LL422 <linle0422@outlook.com>
2023-12-01 21:52:22 -0500

    modify test

--- selenium/src/test/java/edu/pitt/cs/ConnectTest.java
+++ selenium/src/test/java/edu/pitt/cs/ConnectTest.java
@@ -37,6 +37,9 @@ public class ConnectTest {
   JavascriptExecutor js;
   @Before
   public void setUp() {
+    driver = new ChromeDriver();
+    js = (JavascriptExecutor) driver;
+    vars = new HashMap<String, Object>();
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--headless");
     driver = new ChromeDriver(options);
