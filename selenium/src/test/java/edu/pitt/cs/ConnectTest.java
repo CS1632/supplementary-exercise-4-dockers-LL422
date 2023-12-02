85319435411a419bb241ea6b40c9100089f66db1
LL422 <linle0422@outlook.com>
2023-12-01 21:50:24 -0500

    change

--- selenium/src/test/java/edu/pitt/cs/ConnectTest.java
+++ selenium/src/test/java/edu/pitt/cs/ConnectTest.java
@@ -37,9 +37,9 @@ public class ConnectTest {
   JavascriptExecutor js;
   @Before
   public void setUp() {
-    driver = new ChromeDriver();
-    js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless");
+    driver = new ChromeDriver(options);
   }
   @After
   public void tearDown() {
