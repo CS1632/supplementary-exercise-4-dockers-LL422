7073f26555057876bdb55a2bea5c4ed4eedd4bea
LL422 <linle0422@outlook.com>
2023-12-01 22:15:01 -0500

    update the test

--- selenium/src/test/java/edu/pitt/cs/ConnectTest.java
+++ selenium/src/test/java/edu/pitt/cs/ConnectTest.java
@@ -37,9 +37,6 @@ public class ConnectTest {
   JavascriptExecutor js;
   @Before
   public void setUp() {
-    driver = new ChromeDriver();
-    js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--headless");
     driver = new ChromeDriver(options);
--- selenium/src/test/java/edu/pitt/cs/D3Test.java
+++ selenium/src/test/java/edu/pitt/cs/D3Test.java
@@ -35,9 +35,6 @@ public class D3Test {
   JavascriptExecutor js;
   @Before
   public void setUp() {
-    driver = new ChromeDriver();
-    js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--headless");
     driver = new ChromeDriver(options);
